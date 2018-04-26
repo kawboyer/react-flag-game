1cdd343708579946dce850a2c90ea6999cfb9601
Klara Boyer <kawboyer@gmail.com>
2018-04-25 22:07:56 -0400

    tried to add a Counter component

---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Counter extends React.Component {
+  state = {
+    count: 0
+  };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1});
+  };
+}
+
+export default Counter;
